d1b7853fa843628c6706a0f903ba2a47a5dd90f8
Henry <henc313@gmail.com>
2019-12-07 15:30:46 +0200

    Initial Commit

---
+++ PocoSysCodingChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PocoSysCodingChallenge
+//
+//  Created by ♏︎ on 12/7/19.
+//  Copyright © 2019 Henry Kivimaa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view.
+   }
+
+
+}
+
